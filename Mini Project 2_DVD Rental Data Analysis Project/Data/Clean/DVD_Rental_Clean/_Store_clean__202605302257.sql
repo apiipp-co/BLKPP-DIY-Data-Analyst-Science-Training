f6a4302e40f5b21cfff9e61488e0709b061edce7
Adhitya Afif Ardana <168333169+apiipp-co@users.noreply.github.com>
2026-05-30 23:25:40 +0700

    Add files via upload

---
+++ Mini Project 2_DVD Rental Data Analysis Project/Data/Clean/DVD_Rental_Clean/_Store_clean__202605302257.sql
@@ -0,0 +1,3 @@
+INSERT INTO public."Store_clean" (store_id,manager_staff_id,address_id) VALUES
+	 (1,1,1),
+	 (2,2,2);
---
+++ Mini Project 2_DVD Rental Data Analysis Project/Data/Clean/DVD_Rental_Clean/language_clean_202605302257.sql
@@ -0,0 +1,7 @@
+INSERT INTO public.language_clean (language_id,"name") VALUES
+	 (1,'English             '),
+	 (2,'Italian             '),
+	 (3,'Japanese            '),
+	 (4,'Mandarin            '),
+	 (5,'French              '),
+	 (6,'German              ');
