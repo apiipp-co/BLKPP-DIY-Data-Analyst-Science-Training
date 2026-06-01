a178f6d499878cc28a5aead6c116a74f9f7fa2bf
Adhitya Afif Ardana <168333169+apiipp-co@users.noreply.github.com>
2026-06-01 11:37:17 +0700

    Delete Mini Project 2_DVD Rental Data Analysis Project/Data/Clean/DVD_Rental_Clean directory

--- Mini Project 2_DVD Rental Data Analysis Project/Data/Clean/DVD_Rental_Clean/_Store_clean__202605302257.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO public."Store_clean" (store_id,manager_staff_id,address_id) VALUES
-	 (1,1,1),
-	 (2,2,2);
--- Mini Project 2_DVD Rental Data Analysis Project/Data/Clean/DVD_Rental_Clean/language_clean_202605302257.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO public.language_clean (language_id,"name") VALUES
-	 (1,'English             '),
-	 (2,'Italian             '),
-	 (3,'Japanese            '),
-	 (4,'Mandarin            '),
-	 (5,'French              '),
-	 (6,'German              ');
